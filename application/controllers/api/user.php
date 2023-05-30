5662e88e8f03e2c4840f2f0e07458dbba497fe64
bukantio <andantiokorah@gmail.com>
2023-05-30 17:34:50 +0800

    change password

--- application/controllers/api/user.php
+++ application/controllers/api/user.php
@@ -34,4 +34,49 @@ class user extends RestController
         }
     }
 
+    public function changePassword_post(){
+        $res = $this->general_library->validateParam(['old_password', 'new_password', 'confirm_new_password'], 'POST');
+        if($res['code'] != 200){
+            $this->response($res, $res['code']);
+        } else {
+            $req = $this->input->post();
+            $user = $this->m_user->checkUserCredentials($req);
+            if($user){
+                $old_password = $this->general_library->encrypt($req['username'], $req['old_password']);
+                $new_password = $this->general_library->encrypt($req['username'], $req['new_password']);
+                $confirm_new_password = $this->general_library->encrypt($req['username'], $req['confirm_new_password']);
+
+                if($user['password'] == $old_password){
+                    if($new_password == $confirm_new_password){
+                        if(strlen($req['new_password']) >= 6){
+                            $res['code'] = 200;
+                            $res['status'] = true;
+                            $res['data'] = $user;
+                            $res['message'] = "Password Berhasil diubah";
+                            // if($user['logo']){
+                            //     $user['logo'] = imageToBase64(URI_UPLOAD_LOGO_MERCHANT.'logo_merchant/'.$user['logo']);
+                            // }
+                        } else {
+                            $res['code'] = 400;
+                            $res['status'] = false;
+                            $res['message'] = "Password harus lebih dari 6 karakter";
+                        }
+                    } else {
+                        $res['code'] = 400;
+                        $res['status'] = false;
+                        $res['message'] = "Password Baru dan Konfirmasi Password Baru tidak sama";    
+                    }
+                } else {
+                    $res['code'] = 400;
+                    $res['status'] = false;
+                    $res['message'] = "Password Lama salah";
+                }
+            } else {
+                $res['code'] = 404;
+                $res['message'] = "User tidak ditemukan";
+            }
+            $this->response($res, $res['code']);
+        }
+    }
+
 }
